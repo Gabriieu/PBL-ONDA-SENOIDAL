ed9f51b337382f1f6080e626669656e56755c858
José Gabriel <gabriel.souzamorais@gmail.com>
2024-10-12 18:44:14 -0300

    feat: improve pdf & add time validation

--- PBL-EC3/src/main/java/com/example/demo/controller/JasperController.java
+++ PBL-EC3/src/main/java/com/example/demo/controller/JasperController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
 import java.io.IOException;
 
 @RestController
@@ -21,12 +20,22 @@ public class JasperController {
 
     @GetMapping
     public void getPDF(@RequestParam Long id, @RequestParam Double time, HttpServletResponse response) throws IOException {
+
+        // Multiplicando por 100 para trabalhar com inteiros
+        int scaledTime = (int) Math.round(time * 100);
+
+        // Verificando se o valor é múltiplo de 5 (0.05 * 100 = 5)
+        if (scaledTime % 5 != 0) {
+            response.sendError(400, "O tempo deve ser múltiplo de 0.05");
+            return;
+        }
+
         byte[] bytes = jasperService.exportPDF(id, time);
 
         response.setContentType(MediaType.APPLICATION_PDF_VALUE);
         response.setHeader("Content-Disposition", "inline; filename=report.pdf");
         response.setContentLength(bytes.length);
         response.getOutputStream().write(bytes);
-
     }
+
 }
