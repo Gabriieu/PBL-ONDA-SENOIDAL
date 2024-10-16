46aeaf4e20fff4edda4d621eb9515a608fff68bc
José Gabriel <gabriel.souzamorais@gmail.com>
2024-10-16 13:07:09 -0300

    feat: move dashboard & abre automaticamente com API

---
+++ PBL-EC3/src/main/java/com/example/demo/config/ExcelLauncher.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+public class ExcelLauncher implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Usar ClassPathResource para obter o arquivo do classpath
+        File file = new ClassPathResource("excel/Dashboard.xlsm").getFile();
+        String filePath = file.getAbsolutePath();
+
+        // Comando para abrir o Excel com a planilha
+        Runtime.getRuntime().exec("cmd /c start excel \"" + filePath + "\"");
+
+        System.out.println("Planilha Excel iniciada");
+    }
+}
+
+
