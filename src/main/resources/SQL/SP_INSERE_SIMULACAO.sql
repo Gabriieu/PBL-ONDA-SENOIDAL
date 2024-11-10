1d3c596a34ab06bfad0499ca3ae6dcaed104d370
José Gabriel <gabriel.souzamorais@gmail.com>
2024-11-10 10:55:00 -0300

    refactor: add user field

--- src/main/resources/SQL/SP_INSERE_SIMULACAO.sql
+++ src/main/resources/SQL/SP_INSERE_SIMULACAO.sql
@@ -3,12 +3,13 @@ CREATE OR ALTER PROCEDURE SPINSERESIMULACAO
     @comprimentoOnda FLOAT,
     @duracao INT,
     @erroMax FLOAT,
+	@user BIGINT,
     @SimulacaoID BIGINT OUTPUT
 AS
 BEGIN
     -- Inserir a simulação com arredondamento do período
-    INSERT INTO simulacoes (frequencia, comprimento_onda, duracao, erro_max, data)
-    VALUES (@frequencia, @comprimentoOnda, @duracao, @erroMax, GETDATE());
+    INSERT INTO simulacoes (frequencia, comprimento_onda, duracao, erro_max, data, user_id)
+    VALUES (@frequencia, @comprimentoOnda, @duracao, @erroMax, GETDATE(), @user);
 
     -- Retornar o ID gerado
     SET @SimulacaoID = SCOPE_IDENTITY();
