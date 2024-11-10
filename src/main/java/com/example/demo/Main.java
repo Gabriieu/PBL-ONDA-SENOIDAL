aa47e3e3a90365b3047f52141080881a333fe8f2
José Gabriel <gabriel.souzamorais@gmail.com>
2024-11-10 10:51:23 -0300

    add console

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -8,6 +8,11 @@ public class 	Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+		System.out.println("==============================================================================");
+		System.out.println("A aplicação foi iniciada...");
+		System.out.println("Faça o download da planilha acessando http://localhost:8080/dashboard/download");
+		System.out.println("Certifique-se de habilitar as macros para visualizar os dados.");
+		System.out.println("==============================================================================");
 	}
 
 }
